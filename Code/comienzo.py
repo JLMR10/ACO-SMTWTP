1ad8826c656831b24173e08d6dcbb336375a2934
JLMR10 <pepelu101996@gmail.com>
2018-12-28 10:47:43 +0000

    Job class and TEDD

--- Code/comienzo.py
+++ Code/comienzo.py
@@ -1,3 +1,5 @@
+import math,operator
+
 class Ant(object):
     
     def __init__(self,numero):
@@ -8,24 +10,37 @@ class Ant(object):
         self.listaIteracion = []
         self.actualEdge = numero
 
-    def getActualEdge(self):
-        return self.actualEdge
-
-    def getSolution(self):
-        return self.solution
-
-    def getCost(self):
-        return self.cost
-    
-    def getBenefit(self):
-        return self.benefit
-    
-    def getUnifo(self):
-        return self.unifo
-    
-    def getListaIteracion(self):
-        return self.listaIteracion
-
     def __str__(self):
-        return ("esta hormiga tiene un coste de %s y un beneficion de %s con la solucion: %s",(self.cost,self.benefit,self.solution))
-        
+        return ("esta hormiga tiene un coste de %s y un beneficio de %s con la solución: %s",(self.cost,self.benefit,self.solution))
+
+
+class Job(object):
+
+    def __init__(self,numero,pT,w,dD):
+        self.processingTime = pT
+        self.weigth = w
+        self.dueDate = dD
+        self.nodo = numero
+    
+def earliestDueDate(jobList):
+    return sorted(jobList, key=operator.attrgetter('dueDate'))
+
+def totalTardiness(jobList):
+    totalFlowTime = 0
+    flowTimeOut = []
+    earliness = [0]*len(jobList)
+    tardiness = [0]*len(jobList)
+    for i,job in enumerate(jobList):
+        totalFlowTime += job.processingTime
+        flowTimeOut.append(totalFlowTime)
+        lateness = totalFlowTime-job.dueDate
+        if lateness >= 0:
+            tardiness[i] = lateness
+        else:
+            earliness[i] = lateness
+    return sum(tardiness)
+
+problema = [Job(1,26,1,179),Job(2,24,10,183),Job(3,79,9,196),Job(4,46,4,202),Job(5,32,3,192)]
+
+
+
