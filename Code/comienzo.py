54e1b2e91b02026f5550f6fa37a0f358f82a2998
JLMR10 <pepelu101996@gmail.com>
2018-12-28 16:07:16 +0000

    MDD

--- Code/comienzo.py
+++ Code/comienzo.py
@@ -1,4 +1,5 @@
-import math,operator
+import math,operator,copy
+import networkx as nx
 
 class Ant(object):
     
@@ -22,6 +23,10 @@ class Job(object):
         self.dueDate = dD
         self.nodo = numero
     
+    def __str__(self):
+        return ("Job")
+
+    
 def earliestDueDate(jobList):
     return sorted(jobList, key=operator.attrgetter('dueDate'))
 
@@ -40,7 +45,43 @@ def totalTardiness(jobList):
             earliness[i] = lateness
     return sum(tardiness)
 
-problema = [Job(1,26,1,179),Job(2,24,10,183),Job(3,79,9,196),Job(4,46,4,202),Job(5,32,3,192)]
+def totalWeightedTardiness(jobList):
+    totalFlowTime = 0
+    flowTimeOut = []
+    earliness = [0]*len(jobList)
+    tardiness = [0]*len(jobList)
+    for i,job in enumerate(jobList):
+        totalFlowTime += job.processingTime
+        flowTimeOut.append(totalFlowTime)
+        lateness = totalFlowTime-job.dueDate
+        if lateness >= 0:
+            tardiness[i] = lateness*job.weigth
+        else:
+            earliness[i] = lateness*job.weigth
+    return sum(tardiness)
 
+def mddOp(processed, job):
+    return max(processed+job.processingTime,job.dueDate)
+
+def mddSort(jobList):
+    unsortedJobList = copy.copy(jobList)
+    sortedJobList = []
+    processed = 0
+    while unsortedJobList:
+        bestJob = unsortedJobList[0]
+        bestMdd = mddOp(processed,bestJob)
+        for job in unsortedJobList:
+            mdd = mddOp(processed,job)
+            if mdd < bestMdd:
+                bestMdd = mdd
+                bestJob = job
+        sortedJobList.append(bestJob)
+        unsortedJobList.remove(bestJob)
+        processed+=bestJob.processingTime
+    return sortedJobList
+
+
+problema = [Job(1,26,1,179),Job(2,24,10,183),Job(3,79,9,196),Job(4,46,4,202),Job(5,32,3,192)]
+vuelos = [Job(1,8,2,12),Job(2,9,5,18),Job(3,5,2,10),Job(4,6,8,14)]
 
 
